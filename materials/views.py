abaabd3fcad70a4b5b4809c91c38b6bf5b0b4ae0
mmrxsky <mmrxsky@gmail.com>
2024-11-12 21:08:13 +0300

    add Эндпоинт для подписки. Пагинация

--- materials/views.py
+++ materials/views.py
@@ -1,10 +1,14 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-from materials.models import Course, Lesson
-from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
+from materials.models import Course, Lesson, Subscription
+from materials.paginators import CustomPagination
+from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer, SubscriptionSerializer
 from users.permissions import IsModerator, IsOwner
+
 
 class CourseViewSet(ModelViewSet):
     def get_queryset(self):
@@ -14,6 +18,7 @@ class CourseViewSet(ModelViewSet):
             return Course.objects.filter(owner=self.request.user)
 
     serializer_class = CourseSerializer
+    pagination_class = CustomPagination
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -40,6 +45,11 @@ class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
     permission_classes = (~IsModerator, IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
+
 
 class LessonListAPIView(ListAPIView):
     def get_queryset(self):
@@ -47,13 +57,9 @@ class LessonListAPIView(ListAPIView):
             return Lesson.objects.all()
         else:
             return Lesson.objects.filter(owner=self.request.user)
+
     serializer_class = LessonSerializer
-
-
-    def perform_create(self, serializer):
-        lesson = serializer.save()
-        lesson.owner = self.request.user
-        lesson.save()
+    pagination_class = CustomPagination
 
 
 class LessonReviewAPIView(RetrieveAPIView):
@@ -61,12 +67,33 @@ class LessonReviewAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsModerator | IsOwner,)
 
+
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsModerator | IsOwner,)
 
+
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsOwner | ~IsModerator,)
+
+
+class SubscriptionCreateAPIView(CreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        course_id = request.data.get('course')
+        course = get_object_or_404(Course, pk=course_id)
+        subs_item = Subscription.objects.filter(user=user, course=course)
+        if subs_item.exists():
+            subs_item.delete()
+            message = 'Подписка удалена'
+        else:
+            Subscription.objects.create(user=user, course=course, sign_up=True)
+            message = 'Подписка добавлена'
+        return Response({'message': message})
