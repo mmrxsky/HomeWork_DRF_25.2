e65f4fb079d78e2ef454cf393129a86922c3eb16
mmrxsky <mmrxsky@gmail.com>
2024-11-12 19:39:46 +0300

    add Создал модели курсов и уроков

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    """
+    Класс для описания модели курсов
+    """
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса", **NULLABLE, help_text="Введите описание курса"
+    )
+    image = models.ImageField(
+        upload_to="materials/course",
+        verbose_name="Превью",
+        **NULLABLE,
+        help_text="Загрузите превью"
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    """
+    Класс для описания модели уроков
+    """
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока", **NULLABLE, help_text="Введите описание урока"
+    )
+    image = models.ImageField(
+        upload_to="materials/lesson",
+        verbose_name="Превью",
+        **NULLABLE,
+        help_text="Загрузите превью"
+    )
+    video_link = models.CharField(
+        max_length=150,
+        **NULLABLE,
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео"
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Курс", related_name="course"
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
