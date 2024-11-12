b5d6973b1a4785db636a401f8f3424985e4d7b2b
mmrxsky <mmrxsky@gmail.com>
2024-11-12 19:48:38 +0300

    add Прописаны пути

---
+++ materials/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from materials.views import CourseViewSet, LessonListAPIView, LessonReviewAPIView, LessonCreateAPIView, \
+    LessonDestroyAPIView, LessonUpdateAPIView
+from materials.apps import MaterialsConfig
+
+app_name = MaterialsConfig.name
+
+router = SimpleRouter()
+router.register('', CourseViewSet)
+
+urlpatterns = [
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonReviewAPIView.as_view(),name='lesson_review'),
+    path('lesson/create/', LessonCreateAPIView.as_view(),name='lesson_create'),
+    path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(),name='lesson_delete'),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(),name='lesson_update'),
+]
+
+urlpatterns += router.urls
