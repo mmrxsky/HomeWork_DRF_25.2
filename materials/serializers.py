fdeb99470de579eaf5a4872c433997c29ce75d8b
mmrxsky <mmrxsky@gmail.com>
2024-11-12 21:29:51 +0300

    add Отображение признака подписки

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson, Subscription
 from materials.validators import VideoValidator
@@ -11,8 +12,19 @@ class LessonSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(source='lesson_count', many=True, read_only=True)
+    sign_up = SerializerMethodField(source='subscription_course', read_only=True)
+
+    def get_sign_up(self, instance):
+        user = self.context['request'].user
+        return Subscription.objects.filter(user=user).filter(course=instance).exists()
 
     class Meta:
         model = Course
@@ -25,12 +37,8 @@ class CourseDetailSerializer(serializers.ModelSerializer):
 
     def get_lesson_count(self, course):
         return Lesson.objects.filter(course=course).count()
+
+
     class Meta:
         model = Course
-        fields = ('lessons', 'title', 'description', 'lesson_count', 'image', 'owner',)
-
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ('sign_up',)
+        fields = ('lessons', 'title', 'description', 'lesson_count', 'image', 'owner', 'subscription_course',)
