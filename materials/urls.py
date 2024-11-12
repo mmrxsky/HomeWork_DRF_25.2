a345284136d1d5a3fb1cc7b1e416b0f3b38b0535
mmrxsky <mmrxsky@gmail.com>
2024-11-12 21:03:20 +0300

    add Прописан путь для создания подписки

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from materials.views import CourseViewSet, LessonListAPIView, LessonReviewAPIView, LessonCreateAPIView, \
-    LessonDestroyAPIView, LessonUpdateAPIView
+    LessonDestroyAPIView, LessonUpdateAPIView, SubscriptionCreateAPIView
 from materials.apps import MaterialsConfig
 
 app_name = MaterialsConfig.name
@@ -16,6 +16,7 @@ urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(),name='lesson_create'),
     path('lesson/<int:pk>/delete/', LessonDestroyAPIView.as_view(),name='lesson_delete'),
     path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view(),name='lesson_update'),
+    path('course_subscription/', SubscriptionCreateAPIView.as_view(),name='course_subscription'),
 ]
 
 urlpatterns += router.urls
