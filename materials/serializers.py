fc598aa0e12a73a7c2c92be73bb76528cb01feb6
mmrxsky <mmrxsky@gmail.com>
2024-11-12 19:59:39 +0300

    add новый сериализер

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
@@ -7,6 +8,17 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_count.all().count()
+
+    class Meta:
+        model = Course
+        fields = ('title', 'description', 'lesson_count')
 
 
 class LessonSerializer(ModelSerializer):
