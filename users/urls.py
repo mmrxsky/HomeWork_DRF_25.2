d9694985e736035a18c398530f6940ee80e0596d
mmrxsky <mmrxsky@gmail.com>
2024-11-12 20:33:43 +0300

    add Пути для авторизации

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, PaymentCreateAPIView, UserModelViewSet
+from users.views import PaymentListAPIView, PaymentCreateAPIView, UserModelViewSet, UserCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -12,4 +12,7 @@ router.register('', UserModelViewSet)
 urlpatterns = [
     path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
     path("create/payment/", PaymentCreateAPIView.as_view(), name="create_payment"),
+    path('register/', UserCreateAPIView.as_view(), name="register"),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ] + router.urls
