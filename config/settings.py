65e939973e7b47564470118a2b2c13149df9ab5f
mmrxsky <mmrxsky@gmail.com>
2024-11-16 19:17:55 +0300

    add docs drf-yasg и настройка

--- config/settings.py
+++ config/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_yasg",
     "users",
     "materials",
     "django_filters",
     "rest_framework_simplejwt",
+
 ]
 
 MIDDLEWARE = [
