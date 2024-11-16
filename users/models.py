82564c5b5afa179c26eddd49b99581cd64e93693
mmrxsky <mmrxsky@gmail.com>
2024-11-16 19:40:22 +0300

    add ссылка на оплату

--- users/models.py
+++ users/models.py
@@ -64,7 +64,7 @@ class Payment(models.Model):
     )
     payment_sum = models.PositiveIntegerField(verbose_name="Сумма оплаты", **NULLABLE)
     payment_method = models.CharField(max_length=50, verbose_name="Способ оплаты")
-
+    payment_url = models.URLField(max_length=450, verbose_name="Ссылка на оплату", **NULLABLE)
     class Meta:
         verbose_name = "Оплата"
         verbose_name_plural = "Оплаты"
